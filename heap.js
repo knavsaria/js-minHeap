2872ef724834bc2904461cb8cb477ea3e3c3455e
KEERAN NAVSARIA <keeran.navsaria@gmail.com>
2019-08-11 13:08:30 +0200

    added remove function and static isHeap checker

--- heap.js
+++ heap.js
@@ -16,12 +16,12 @@ class MinHeap {
     }
 
     getLeftChild(index) {
-        if (index >= this.count) throw (new Error('Out of bounds of heap'));
+        if (index >= this.count) return;
         return this.array[(2*index) + 1];
     }
 
     getRightChild(index) {
-        if (index >= this.count) throw (new Error('Out of bounds of heap'));
+        if (index >= this.count) return;
         return this.array[(2*index) + 2];
     }
 
@@ -33,7 +33,7 @@ class MinHeap {
         if (index <= 0) return;
         if (this.array[this.getParentIndex(index)] > this.array[index]) {
             MinHeap.swap(this.array, index, this.getParentIndex(index));
-            this.shiftUp((index-1)/2);
+            this.shiftUp(this.getParentIndex(index));
         }
     }
 
@@ -44,11 +44,50 @@ class MinHeap {
         this.shiftUp(this.count-1);
     }
 
+    shiftDown(index) {        
+        if (2*index+1 >= this.count && 2*index+2 >= this.count) return;
+
+        while (this.array[index] > this.getLeftChild(index) || this.array[index] > this.getRightChild(index)) {
+            if (this.getLeftChild(index) <= this.getRightChild(index)) {
+                MinHeap.swap(this.array, index, 2*index+1);
+                this.shiftDown(2*index+1);
+            }
+            else {
+                MinHeap.swap(this.array, index, 2*index+2);
+                this.shiftDown(2*index+2);
+            }
+        }
+    }
+
+    remove() {
+        if (this.count <= 0) throw (new Error('HeapUnderflowException'));
+        var min = this.array[0];
+        this.array[0] = this.array[this.count-1];
+        this.count--;
+        this.array.pop();
+        this.shiftDown(0);
+        return min;
+    }
+
+
     getMin() {
         if (this.count == 0) throw (new Error('EmptyHeapException'));
         return this.array[0];
     }
+
     
+
+    static isHeap(array) {
+        function isLessThanKids(index) {
+            if (2*index+1 >= array.length  && 2*index+2 >= array.length) return true;
+            if (array[2*index+1] < array[index] || array[2*index+2] < array[index]) return false;
+            return isLessThanKids(2*index+1) && isLessThanKids(2*index+2);            
+        }
+
+        if (array.length <= 0) throw (new Error('EmptyArrayException'));
+        var result = isLessThanKids(0);        
+        return result;        
+    }
 }
 
 var heap = new MinHeap(30);
@@ -59,5 +98,36 @@ heap.insert(15);
 heap.insert(12);
 heap.insert(7);
 heap.insert(6);
+heap.insert(43);
+heap.insert(23);
+heap.insert(18);
+heap.insert(6);
+heap.insert(68);
+heap.insert(64);
 
 console.log(heap.array);
+console.log(MinHeap.isHeap(heap.array));
+
+heap.remove();
+console.log(heap.array);
+console.log(MinHeap.isHeap(heap.array));
+
+heap.remove();
+console.log(heap.array);
+console.log(MinHeap.isHeap(heap.array));
+
+heap.remove();
+console.log(heap.array);
+console.log(MinHeap.isHeap(heap.array));
+
+heap.remove();
+console.log(heap.array);
+console.log(MinHeap.isHeap(heap.array));
+
+heap.remove();
+console.log(heap.array);
+console.log(MinHeap.isHeap(heap.array));
+
+heap.remove();
+console.log(heap.array);
+console.log(MinHeap.isHeap(heap.array));
